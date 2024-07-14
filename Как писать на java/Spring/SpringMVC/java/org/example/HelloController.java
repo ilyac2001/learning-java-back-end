ca38f03b7c281c1fd87ec328a2fad6a20693e0d1
Илья Цимерман <92425776+ilyac2001@users.noreply.github.com>
2024-07-14 16:37:17 +0300

    Add files via upload

--- Как писать на java/Spring/SpringMVC/java/org/example/HelloController.java
+++ Как писать на java/Spring/SpringMVC/java/org/example/HelloController.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello-world")
-    public String sayHello() {
+    public String sayHello(Model model, HttpServletRequest request) {
+        String param = request.getParameter("message");
+        if(param != null) {
+            model.addAttribute("message", param);
+        } else {
+            model.addAttribute("message", "");
+        }
         return "hello_world";
+
     }
 }
--- Как писать на java/Spring/SpringMVC/java/org/example/controllers/FirstController.java
+++ Как писать на java/Spring/SpringMVC/java/org/example/controllers/FirstController.java
@@ -1,20 +1,25 @@
 package org.example.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hi")
-    public String FirstHello(){
+    public String FirstHello(HttpServletRequest request){
+        String name = request.getParameter("name");
+        System.out.println(name + " " + request.getRequestURI());
         return "first/hello";
     }
 
     @GetMapping("/bye")
-    public String FirstGoodbye() {
+    public String FirstGoodbye(@RequestParam(value = "name", required = false) String name) {
+        System.out.println(name);
         return "first/goodbye";
     }
 }

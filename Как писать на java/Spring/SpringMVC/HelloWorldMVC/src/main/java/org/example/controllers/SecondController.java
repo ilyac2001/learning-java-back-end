d92f288bdebe1be8bed599787d4004ebe9277343
Илья Цимерман <92425776+ilyac2001@users.noreply.github.com>
2024-07-17 12:48:28 +0300

    Add files via upload

---
+++ Как писать на java/Spring/SpringMVC/HelloWorldMVC/src/main/java/org/example/controllers/SecondController.java
@@ -0,0 +1,12 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/second/exit")
+    public String Exit() {
+        return "second/exit";
+    }
+}
